3ad9a20517e780710d8cd301a4260940552b9111
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-04-03 11:50:17 +0700

    separate card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Card from "./Card";
 import Profile from "./Profile";
 import Socials from "./Socials";
 
@@ -7,13 +8,10 @@ export default function App() {
       className="fixed top-0 left-0 h-screen w-screen bg-neutral-950
       flex justify-center items-center text-white"
     >
-      <div
-        className="w-[80%] max-w-96 rounded-2xl bg-neutral-900 p-8 m-8 
-        flex flex-col justify-center items-center gap-4"
-      >
+      <Card>
         <Profile />
         <Socials />
-      </div>
+      </Card>
     </div>
   );
 }
